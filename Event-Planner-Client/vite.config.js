eb77fc2ec0e4da17cc4a8a13282fe489b59064a3
Raxon Suri <raxonsuriperez@gmail.com>
2026-06-10 12:41:16 -0400

    Set up depencencies and routes

--- Event-Planner-Client/vite.config.js
+++ Event-Planner-Client/vite.config.js
@@ -1,11 +1,23 @@
 import { defineConfig } from 'vite'
 import react, { reactCompilerPreset } from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 import babel from '@rolldown/plugin-babel'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    babel({ presets: [reactCompilerPreset()] })
+    babel({ presets: [reactCompilerPreset()] }),
+    tailwindcss(),
   ],
+  server: {
+	    proxy: {
+		    "/api": {
+			    target: "http://localhost:5000",
+		    },
+        "/event": {
+          target: "http://localhost:5000",
+        }
+	    }
+    }
 })
