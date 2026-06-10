bba0a163797c398c15a11b18d82c486560fe9afd
Raxon Suri <raxonsuriperez@gmail.com>
2026-06-10 12:42:32 -0400

    Added font-awesome

--- Event-Planner-Client/src/pages/login.jsx
+++ Event-Planner-Client/src/pages/login.jsx
@@ -1,7 +1,41 @@
-import React from 'react'
+import React, { useState } from "react";
+import axios from "axios";
 
-export const login = () => {
+export const Login = () => {
+  const [loginForm, setLoginForm] = useState({
+    email: "",
+    password: "",
+  });
+
   return (
-    <div>login</div>
-  )
-}
+    <>
+      <form id="container">
+        <h2>Please Login</h2>
+
+        <input
+          type="email"
+          placeholder="Email"
+          value={loginForm.email}
+          onChange={(e) => {
+            setLoginForm({ ...loginForm, email: e.target.value });
+          }}
+        />
+
+        <div>
+          <input
+            type="password"
+            placeholder="Password"
+            value={loginForm.password}
+            onChange={(e) => {
+              setLoginForm({ ...loginForm, password: e.target.value });
+            }}
+          />
+
+          <button>
+            
+          </button>
+        </div>
+      </form>
+    </>
+  );
+};
